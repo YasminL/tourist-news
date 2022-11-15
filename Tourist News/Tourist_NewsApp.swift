4933f9767134a831b12ce1f5ab545bcd0315ee34
Yasmin Lindholm <YasminL@users.noreply.github.com>
2022-11-15 15:25:14 -0500

    Initial Commit

---
+++ Tourist News/Tourist_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tourist_NewsApp.swift
+//  Tourist News
+//
+//  Created by Yasmin Lindholm on 11/15/22.
+//
+
+import SwiftUI
+
+@main
+struct Tourist_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
